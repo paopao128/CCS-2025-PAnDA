aa5eeb5c057617405ff7f925a3051757d7cbd9c4
ruiyao liu <155015957+paopao128@users.noreply.github.com>
2025-09-01 13:46:06 -0500

    Delete functions directory

--- functions/Rn_for_mn.m
+++
@@ -1,4 +0,0 @@
-function Rn_for_mn = Rn_for_mn(m,n,q)
-Rn_for_mn = find(q(m, :) > q(n, :));
-end
-
--- functions/notin_Rn_for_mn.m
+++
@@ -1,3 +0,0 @@
-function notin_Rn_for_mn = notin_Rn_for_mn(m,n,q)
-notin_Rn_for_mn = find(q(m, :) <= q(n, :));
-end
